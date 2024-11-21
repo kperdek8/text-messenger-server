991fb807298325c84999d25c9c82f50338f4b747
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-11-21 14:52:55 +0100

    Cryptographic keys protobuf schemas, adjust message insertion to new schema, group key insertion/fetch methods

--- lib/protos/chat.pb.ex
+++ lib/protos/chat.pb.ex
@@ -52,3 +52,65 @@ defmodule TextMessengerServer.Protobuf.ChatMessages do
 
   field :messages, 1, repeated: true, type: TextMessengerServer.Protobuf.ChatMessage
 end
+
+defmodule TextMessengerServer.Protobuf.GroupKey do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :chat_id, 1, type: :string, json_name: "chatId"
+  field :user_id, 2, type: :string, json_name: "userId"
+  field :key_number, 3, proto3_optional: true, type: :int32, json_name: "keyNumber"
+  field :encrypted_key, 4, type: :bytes, json_name: "encryptedKey"
+end
+
+defmodule TextMessengerServer.Protobuf.GroupKeys do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :group_keys, 1,
+    repeated: true,
+    type: TextMessengerServer.Protobuf.GroupKey,
+    json_name: "groupKeys"
+end
+
+defmodule TextMessengerServer.Protobuf.EncryptionKey do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :user_id, 1, type: :string, json_name: "userId"
+  field :encrypted_key, 2, type: :bytes, json_name: "encryptedKey"
+end
+
+defmodule TextMessengerServer.Protobuf.EncryptionKeys do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :encryption_keys, 1,
+    repeated: true,
+    type: TextMessengerServer.Protobuf.EncryptionKey,
+    json_name: "encryptionKeys"
+end
+
+defmodule TextMessengerServer.Protobuf.SignatureKey do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :user_id, 1, type: :string, json_name: "userId"
+  field :encrypted_key, 2, type: :bytes, json_name: "encryptedKey"
+end
+
+defmodule TextMessengerServer.Protobuf.SignatureKeys do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :signature_keys, 1,
+    repeated: true,
+    type: TextMessengerServer.Protobuf.SignatureKey,
+    json_name: "signatureKeys"
+end
--- lib/text_messenger_server/chats.ex
+++ lib/text_messenger_server/chats.ex
@@ -1,8 +1,8 @@
 defmodule TextMessengerServer.Chats do
   alias TextMessengerServer.Repo
-  alias TextMessengerServer.Chats.{Chat, ChatUser, ChatMessage}
+  alias TextMessengerServer.Chats.{Chat, ChatUser, ChatMessage, GroupKey}
   alias TextMessengerServer.Protobuf
-  alias TextMessengerServer.Accounts.User
+  alias TextMessengerServer.Accounts.{User}
 
   import Ecto.Query
 
@@ -98,20 +98,6 @@ defmodule TextMessengerServer.Chats do
   end
 
   @doc """
-  Inserts a new message into a specified chat.
-  """
-  def insert_chat_message(chat_id, user_id, content) do
-    %ChatMessage{}
-    |> ChatMessage.changeset(%{
-      chat_id: chat_id,
-      user_id: user_id,
-      content: content,
-      timestamp: DateTime.utc_now()
-    })
-    |> Repo.insert()
-  end
-
-  @doc """
   Fetches messages for a specific chat and returns them in Protobuf format.
   """
   def get_chat_messages(chat_id) do
@@ -128,6 +114,86 @@ defmodule TextMessengerServer.Chats do
   end
 
   @doc """
+  Inserts a new message into a specified chat.
+  """
+  # TODO: Remove default iv after implementation of endpoint
+  def insert_chat_message(chat_id, user_id, content, iv \\ <<1, 2, 3, 4>>) do
+    query = from c in TextMessengerServer.Chats.Chat,
+            where: c.id == ^chat_id,
+            select: c.current_key_number
+
+    case Repo.one(query) do
+      nil ->
+        {:error, :chat_not_found}
+
+      current_key_number ->
+        %ChatMessage{}
+        |> ChatMessage.changeset(%{
+          chat_id: chat_id,
+          user_id: user_id,
+          content: content,
+          iv: iv,
+          timestamp: DateTime.utc_now(),
+          key_number: current_key_number
+        })
+        |> Repo.insert()
+    end
+  end
+
+  @doc """
+  Fetch the newest group key for a specific chat and user, sorted by key_number.
+  """
+  def get_latest_group_key(chat_id, user_id) do
+    GroupKey
+    |> where([gk], gk.chat_id == ^chat_id and gk.user_id == ^user_id)
+    |> order_by([gk], desc: gk.key_number)
+    |> limit(1)
+    |> Repo.one()
+  end
+
+  @doc """
+  Fetch all group keys for a specific chat and user, sorted by key_number.
+  """
+  def get_all_group_keys(chat_id, user_id) do
+    GroupKey
+    |> where([gk], gk.chat_id == ^chat_id and gk.user_id == ^user_id)
+    |> order_by([gk], asc: gk.key_number)  # Sort by key_number in ascending order
+    |> Repo.all()
+  end
+
+  @doc """
+  Changes group key by inserting it and incrementing key number.
+  """
+  def change_group_key(chat_id, %Protobuf.GroupKeys{group_keys: group_keys}) do
+    Repo.transaction(fn ->
+      # 1. Check if group_keys has one key per each chat member
+      users_count = get_chat_users_count(chat_id)
+      group_keys_count = length(group_keys)
+
+      if group_keys_count != users_count do
+        {:error, :key_member_count_mismatch}
+      else
+        # 2. Increment the group key number for the chat
+        case increment_key_number(chat_id) do
+          {:error, reason} ->
+            # Rollback and return error if incrementing key number fails
+            {:error, reason}
+          {:ok, key_number} ->
+            # 3. Insert the group keys into the database
+            case insert_group_keys(chat_id, group_keys, key_number) do
+              :ok ->
+                # Return success if all group keys were inserted
+                :group_keys_inserted
+              {:error, reason} ->
+                # Rollback and return error if insertion fails
+                {:error, reason}
+            end
+        end
+      end
+    end)
+  end
+
+  @doc """
   Verifies if user is member of specific chat.
   """
   def is_user_member_of_chat?(user_id, chat_id) do
@@ -137,6 +203,58 @@ defmodule TextMessengerServer.Chats do
             select: u.id
 
     Repo.exists?(query)
+  end
+
+  # Private functions
+
+  # Fetch number of users in the chat
+  defp get_chat_users_count(chat_id) do
+    Repo.one(
+      from cu in ChatUser,
+      where: cu.chat_id == ^chat_id,
+      select: count(cu.user_id)
+    )
+  end
+
+  # Increment key number for specified chat
+  defp increment_key_number(chat_id) do
+    from(c in TextMessengerServer.Chats.Chat,
+      where: c.id == ^chat_id,
+      update: [set: [current_key_number: c.current_key_number + 1]],
+      select: c.current_key_number
+    )
+    |> Repo.update_all([])
+    |> case do
+      {0, _} -> {:error, :chat_not_found}
+      {1, [new_key_number]} -> {:ok, new_key_number}
+      _ -> {:error, :unexpected_result}
+    end
+  end
+
+  # Insert the group keys into the database for each user
+  defp insert_group_keys(chat_id, group_keys, key_number) do
+    Enum.each(group_keys, fn %Protobuf.GroupKey{user_id: user_id, encrypted_key: key} ->
+      case insert_group_key(chat_id, user_id, key, key_number) do
+        :ok -> :ok
+        {:error, reason} -> {:error, reason}
+      end
+    end)
+  end
+
+  # Insert a new group key into the database
+  defp insert_group_key(chat_id, user_id, encrypted_key, key_number) do
+    %GroupKey{}
+    |> GroupKey.changeset(%{
+      chat_id: chat_id,
+      user_id: user_id,
+      encrypted_key: encrypted_key,
+      key_number: key_number
+    })
+    |> Repo.insert()
+    |> case do
+      {:ok, _group_key} -> :ok
+      {:error, %{errors: errors}} -> {:error, "Failed to insert group key: #{errors}"}
+    end
   end
 
   # Conversion Functions
